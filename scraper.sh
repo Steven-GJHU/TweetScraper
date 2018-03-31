9c97c627ecaf9e3d008986dfcbac3558988f8d7a
Steven-GJHU <32387673+Steven-GJHU@users.noreply.github.com>
2018-03-31 17:38:52 +0800

    Add files via upload

--- scraper.sh
+++ scraper.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 location=$1
-fname=$2
-if [[ -z ${fname} ]]; then
+if [[ -z ${location} ]]; then
  echo "Need file containing query"
  exit
 fi
@@ -12,17 +11,22 @@ if [[ ! -d ${logdir} ]]; then
 fi
 
 # scrapy crawl TweetScraper -a query=thor near "manhatan"
-cat ${fname} | while read line
+genrelist=`ls Genre/`
+for fname in ${genrelist}
 do
- c=`echo $line | awk '{split($0,a,";" ); print a[1]}'`
- t=`echo $line | awk '{split($0,a,";" ); print a[2]}'`
- query=""${c}" near:\""${location}"\" since:"${t}"-01-01"
- filename=${query//:/-}
- #echo ${c}
- #echo ${t}
- #echo ${query}
- #echo ${filename}
- echo -e "Start crawl ${query}... with shell\n\t (scrapy crawl TweetScraper -a query="${query}" -a lang=en -a filename="${filename//" "/_}" -a location="${location}" -a category="${fname}"> "${logdir}${filename}.log" 2>&1)"
- scrapy crawl TweetScraper -a query="${query}" -a lang=en -a filename="${filename//" "/_}" -a location="${location}" -a category="${fname}" > "${logdir}${filename}.log" 2>&1
+  cat Genre"/"${fname} | while read line
+  do
+   c=`echo $line | awk '{split($0,a,";" ); print a[1]}'`
+   t=`echo $line | awk '{split($0,a,";" ); print a[2]}'`
+   query=""${c}" near:\""${location}"\" since:"${t}"-01-01"
+   filename=${query//:/-}
+   #echo ${c}
+   #echo ${t}
+   #echo ${query}
+   #echo ${filename}
+   echo -e "Start crawl ${query}... with shell\n\t (scrapy crawl TweetScraper -a query="${query}" -a lang=en -a filename="${filename//" "/_}" -a location="${location}" -a category="${fname}"> "${logdir}${filename}.log" 2>&1)"
+   scrapy crawl TweetScraper -a query="${query}" -a lang=en -a filename="${filename//" "/_}" -a location="${location}" -a category="${fname}" > "${logdir}${filename}.log" 2>&1
+   wait
+  done
  wait
 done
